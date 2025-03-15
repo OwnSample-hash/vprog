0752796d53f818f3147650133a8edf3824719863
OwnSample <ownsample@tutanota.com>
2025-03-15 20:50:41 +0100

    Users class for the user in the db

---
+++ User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace car {
+  enum EPermission {
+    User = 0,
+    Seller = 1,
+    Admin = 2
+  }
+
+  [Table("Users")]
+  class User {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; } = 0;
+    public string Username { get; set; } = "";
+
+    public string Password { get; set; } = "";
+
+    private EPermission _Permission { get; set; } = EPermission.User;
+    public int PermissionId {
+      get => (int)this._Permission;
+      set => this._Permission = (EPermission)value;
+    }
+  }
+}
