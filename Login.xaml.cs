5350ab6a124a759c728c61f1fc4f450e71c2b9ae
OwnSample <ownsample@tutanota.com>
2025-03-16 21:04:53 +0100

    Use new session element

--- Login.xaml.cs
+++ Login.xaml.cs
@@ -1,28 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using car.Session;
 
 namespace car {
   /// <summary>
   /// Interaction logic for Login.xaml
   /// </summary>
-  public partial class Login : Window {
-    public Login() {
+  public sealed partial class Login : Window {
+
+    public string Username { get => tbNev.Text; }
+    public string Password { get => pbJelszo.Password; }
+
+    public Login(ESessionAuthError eSessionAuthError = ESessionAuthError.OK) {
       InitializeComponent();
+      switch (eSessionAuthError) {
+        case ESessionAuthError.InvalidCredentials:
+          MessageBox.Show("Hibás felhasználónév vagy jelszó!");
+          break;
+        case ESessionAuthError.GeneralError:
+          MessageBox.Show("Hiba történt!");
+          break;
+      }
+      //var t = new DispatcherTimer { Interval = new(0, 0, 0, 0, 50) };
+      //t.Tick += (s, e) => {
+      //  t.Stop();
+      //  DialogResult = false;
+      //};
+      //t.Start();
     }
 
     private void btnLogin_Click(object sender, RoutedEventArgs e) {
-
+      if (tbNev.Text != "" && pbJelszo.Password != "") {
+        DialogResult = true;
+      } else {
+        MessageBox.Show("A mezőket ki kell tölteni!");
+      }
     }
   }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,29 +8,28 @@ namespace car;
 /// Interaction logic for MainWindow.xaml
 /// </summary>
 public partial class MainWindow : Window {
+
+  public static string conString = File.ReadAllText("connectionString.txt", Encoding.UTF8);
+
+  public static bool verbose = Environment.GetCommandLineArgs().Any((e) => e == "--verbose");
   public MainWindow() {
     InitializeComponent();
     var args = Environment.GetCommandLineArgs();
     if (args.Length > 1 && args.Any((e) => e == "--migrate")) {
-      var conString = File.ReadAllText("connectionString.txt", Encoding.UTF8);
-      var migration = new DB.MigrationManager(conString, args.Any((e) => e == "--verbose"));
+      var migration = new DB.MigrationManager(conString, verbose);
       if (migration.Migrate(args.Any((e) => e == "--down"))) {
-        MessageBox.Show("Migráció sikeres");
-      }
-      else {
+        //MessageBox.Show("Migráció sikeres");
+      } else {
         MessageBox.Show("Migráció sikertelen");
       }
     }
     if (args.Length > 1 && args.Any((e) => e == "--seed")) {
       var conString = File.ReadAllText("connectionString.txt", Encoding.UTF8);
-      var migration = new DB.MigrationManager(conString, args.Any((e) => e == "--verbose"));
+      var migration = new DB.MigrationManager(conString, verbose);
       if (migration.Seed()) {
-        MessageBox.Show("Adatok betöltése sikeres");
-      }
-      else {
+      } else {
         MessageBox.Show("Adatok betöltése sikertelen");
       }
     }
-    //new Login().Show();
   }
 }
