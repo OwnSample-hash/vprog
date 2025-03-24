b6099e16fabeee383f4f71feb87bc9be0cc62fd7
OwnSample <ownsample@tutanota.com>
2025-03-24 17:48:46 +0100

    Debug stuff

---
+++ DebugUtily/AllocConsoleOnVerbose.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.InteropServices;
+
+namespace car.DebugUtily {
+  /// <summary>
+  /// Allocate Console If Verbose argument is passed
+  /// </summary>
+  public static class AllocConsoleOnVerbose {
+
+    [DllImport("kernel32.dll")]
+    private static extern bool AllocConsole();
+
+    [DllImport("kernel32.dll")]
+    private static extern bool FreeConsole();
+
+    /// <summary>
+    /// Allocates The Console and redirects the output to it
+    /// </summary>
+    public static void DoAllocConsoleOnVerbose() {
+      AllocConsole();
+      Console.SetOut(new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = true });
+    }
+
+    /// <summary>
+    /// Frees the console
+    /// </summary>
+    public static void DoFreeConsole() {
+      FreeConsole();
+    }
+  }
+}
---
+++ DebugUtily/ShiftHoldBypassDown.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+namespace car.DebugUtily {
+
+  /// <summary>
+  /// Check if the shift key is held down
+  /// </summary>
+  public static class ShiftHoldBypassDown {
+
+    [DllImport("User32.dll")]
+    private static extern short GetAsyncKeyState(int code);
+
+    /// <summary>
+    /// Check if the shift key is held down
+    /// </summary>
+    /// <returns>Returns true if either of shitfs are held </returns>
+    public static bool IsShiftDown() {
+      return GetAsyncKeyState(0x10) != 0;
+    }
+  }
+}
