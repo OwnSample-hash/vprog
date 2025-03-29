3a89e3e7dedfa31c9aa11778fca190eb2848d5be
OwnSample <ownsample@tutanota.com>
2025-03-29 21:55:06 +0100

    Added system with proper perm

--- Seeds/001_Permissions.sql
+++ Seeds/001_Permissions.sql
@@ -1,5 +1,6 @@
 INSERT INTO Permissions (Id, Permission) VALUES 
   (0, 'User'),
   (1, 'Seller'),
-  (2, 'Admin')
+  (2, 'Admin'),
+  (0x8AAA,'System')
 ;
--- Seeds/002_Users.sql
+++ Seeds/002_Users.sql
@@ -1,5 +1,5 @@
 INSERT INTO Users (Username, Password, PermissionId) VALUES
-  ('System', '', 2),
+  ('System', '', 0x8AAA),
   ('admin', 'admin', 2),
   ('seller', 'seller', 1),
   ('user', 'user', 0)
