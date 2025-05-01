aae19f0f40e885f8546893018c14894ece8b83bf
OwnSample <ownsample@tutanota.com>
2025-05-01 17:14:12 +0200

    Starting balance

--- Seeds/002_Users.sql
+++ Seeds/002_Users.sql
@@ -1,9 +1,9 @@
-INSERT INTO Users (Username, Password, PermissionId) VALUES
-  ('System', '', 0x8AAA),
-  ('admin', 'admin', 2),
-  ('seller', 'seller', 1),
-  ('seller2', 'seller', 1),
-  ('seller3', 'seller', 1),
-  ('seller4', 'seller', 1),
-  ('user', 'user', 0)
+INSERT INTO Users (Username, Password, PermissionId, Balance) VALUES
+  ('System', '', 0x8AAA,0),
+  ('admin', 'admin', 2,100000),
+  ('seller', 'seller', 1,1000),
+  ('seller2', 'seller', 1,10),
+  ('seller3', 'seller', 1,10),
+  ('seller4', 'seller', 1,0),
+  ('user', 'user', 0,10000)
 ;
